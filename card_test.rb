aaf6439f650c1daf1be3b8f7f4074f0fd4dc6f2e
Rob <epr1935@icloud.com>
2017-02-08 23:41:46 -0500

    bit more work on tests

--- card_test.rb
+++ card_test.rb
@@ -6,12 +6,12 @@ class CardTest < MiniTest::Test
 
   def setup
     @card = Card.new
+    @card.value  = 11
+    @card.face   = 'Jack'
+    @card.suit   = 'Spade'
   end
 
   def test_card_not_nil
-    @card.suit  = 'Heart'
-    @card.face  = 'Ace'
-    @card.value = 14
     assert @card.suit  != nil
     assert @card.value != nil
     assert @card.face  != nil
@@ -19,33 +19,21 @@ class CardTest < MiniTest::Test
 
   # An Ace has a value of 14
   def test_Ace_is_14
-    @card.suit  = 'Heart'
-    @card.face  = 'Ace'
-    @card.value = 14
     assert @card.value == 14
   end
 
   # A King has a value of 13
   def test_King_is_13
-    @card.suit  = 'Heart'
-    @card.face  = 'King'
-    @card.value = 13
     assert @card.value == 13
   end
 
   # A Queen has a value of 12
   def test_Queen_is_12
-    @card.suit  = 'Heart'
-    @card.face  = 'Queen'
-    @card.value = 12
     assert @card.value == 12
   end
 
   # A Jack has a value of 11
   def test_Jack_is_11
-    @card.suit  = 'Heart'
-    @card.face  = 'Jack'
-    @card.value = 11
     assert @card.value == 11
   end
 
--- deck_test.rb
+++ deck_test.rb
@@ -9,7 +9,29 @@ class DeckTest < MiniTest::Test
   end
 
   def test_deck_is_52
-    assert @newdeck.deck.length == 52
+    assert @deck.deck.length == 52
   end
 
+# Yet to be completed !!!
+# A deck contains 13 of each suit
+  def test_contains_13_eachsuit
+    assert true == false
+  end
+
+# A deck contains four of each face of cards
+  def test_contains_four_eachface
+    assert true == false
+  end
+
+# A deck is shuffled on creation
+  def test_shuffled
+    assert true == false
+  end
+
+  def test_draw_reduces_deck
+    before = @deck.length
+    junk = @deck.draw
+    after = @deck.length
+    assert after < before
+  end
 end
